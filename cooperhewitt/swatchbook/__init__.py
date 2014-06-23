b01c0ebf1d2b1a90a4fc6f4c6e6371f6e0b93b84
copea <copea@si.edu>
2014-06-23 11:38:39 -0400

    start defining palettes in code

--- cooperhewitt/swatchbook/__init__.py
+++ cooperhewitt/swatchbook/__init__.py
@@ -5,7 +5,7 @@ import webcolors
 import json
 import sys
 
-class palette:
+class swatchbook:
 
     def __init__(self, path):
 
@@ -87,37 +87,6 @@ class palette:
            
         json.dump(out, fh, indent=2)
 
-class utils:
-
-    def __init__(self, palette):
-
-        self.palette = palette
-
-    # http://stackoverflow.com/questions/9694165/convert-rgb-color-to-english-color-name-like-green
-
-    def closest_colour(self, hex):
-
-        r, g, b = webcolors.hex_to_rgb(hex)
-
-        min_colours = {}
-
-        plt = self.palette
-
-        for key, name in plt.colours.items():
-
-            r_c, g_c, b_c = webcolors.hex_to_rgb(key)
-            rd = (r_c - r) ** 2
-            gd = (g_c - g) ** 2
-            bd = (b_c - b) ** 2
-            min_colours[(rd + gd + bd)] = name
-
-        idx = min(min_colours.keys())
-
-        details = min_colours[idx]
-        name = details['name']
-
-        hex = plt.name_to_hex(name)
-        return hex, name
 
 if __name__ == '__main__':
 
--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ from distutils.core import setup
 
 setup(name='cooperhewitt-swatchbook',
       version='0.1',
-      description='Cooper Hewitt's Python tools for wrangling colours',
+      description='Cooper Hewitt\'s Python tools for wrangling colours',
       author='Cooper Hewitt Smithsonian Design Museum',
       url='https://github.com/cooperhewitt/py-cooperhewitt-swatchbook',
       requires=['colorsys', 'webcolors'],
       packages=[
-          'cooperhewitt'
+          'cooperhewitt',
+          'cooperhewitt.swatchbook'
       ],
       scripts=[],
       download_url='https://github.com/cooperhewitt/py-cooperhewitt-swatchbook/releases/tag/v0.1',
