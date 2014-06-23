74a182a13bae5dde3e2b4fe604a839e5ec05e771
copea <copea@si.edu>
2014-06-23 11:54:03 -0400

    load palette hoohah

--- cooperhewitt/swatchbook/palettes/__init__.py
+++ cooperhewitt/swatchbook/palettes/__init__.py
@@ -1,1 +1,25 @@
-# I blame, Guido
+def palettes():
+
+    return [
+        'css3',
+        'css4'
+    ]
+
+def load_palette(reference):
+
+    if not reference in palettes():
+        raise Exception, "Invalid palette"
+
+    # Please figure out the hoo-hah to make dynamic
+    # loading work (20140623/straup)
+
+    if reference == 'css3':
+        import css3
+        return css3.colours()
+    
+if __name__ == '__main__':
+
+    p = load_palette('css5')
+    print p
+    
+    
